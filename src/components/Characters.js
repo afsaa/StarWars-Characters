79b8bd02356dccd2128b0d6eab42ebb1d7cb1c07
Andrés Saam <andressaa.dev@gmail.com>
2019-11-30 16:14:56 -0500

    Improving functions and adding a extra constant and state

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from "react";
 
 const Characters = props => {
+  const URL = `https://swapi.co/api/people/`;
+  const [executed, setExecuted] = useState(false);
   const [count, setCount] = useState(0);
   const [characters, setCharacters] = useState([]);
   const [prevCharacter, setPrevCharacter] = useState({});
   const [currentCharacter, setCurrentCharacter] = useState({});
-  const [NextCharacter, setNextCharacter] = useState({});
+  const [nextCharacter, setNextCharacter] = useState({});
+
+  function getData() {
+    if (!executed) {
+      fetch(URL)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(data => {
+          setCharacters(data.results);
+          console.log(data.results);
+        });
+      setExecuted(true);
+    }
+  }
 
   function handleClick(e) {
     if (e.target.id === "leftBtn") {
-      setPrevCharacter(characters[count - 1]);
-      setCurrentCharacter(characters[count]);
-      setNextCharacter(characters[count + 1]);
       setCount(count - 1);
     }
     if (e.target.id === "rightBtn") {
-      setPrevCharacter(characters[count - 1]);
-      setCurrentCharacter(characters[count]);
-      setNextCharacter(characters[count + 1]);
       setCount(count + 1);
     }
   }
+
   useEffect(() => {
-    fetch(`https://swapi.co/api/people/`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(data => {
-        setCharacters(data.results);
-        console.log(data.results);
-      });
+    getData();
+    setPrevCharacter(characters[count - 1]);
+    setCurrentCharacter(characters[count]);
+    setNextCharacter(characters[count + 1]);
   }, [count]);
 
   return (
     <div>
-      <button id="leftBtn" onClick={e => handleClick(e)}>
+      <button id="leftBtn" disabled={count === 0} onClick={e => handleClick(e)}>
         Previous Character
       </button>
       <div>
         <h3>{currentCharacter ? currentCharacter.name : ""}</h3>
       </div>
-      <button id="rightBtn" onClick={e => handleClick(e)}>
+      <button
+        id="rightBtn"
+        disabled={count === characters.length - 1}
+        onClick={e => handleClick(e)}
+      >
         Next Character
       </button>
     </div>
