c467b85141a769f9aa1dbf7dbfb1228af6edbabe
Andrés Saam <andressaa.dev@gmail.com>
2019-12-03 09:41:19 -0500

    Creating and styling the header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style/Header.css";
+const Header = () => (
+  <div className="Header">
+    <h1>Star Wars Characters</h1>
+  </div>
+);
+
+export default Header;
