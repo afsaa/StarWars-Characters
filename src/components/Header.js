270cccdcc268c47b00c35ef16b0ae36224062024
Andrés Saam <andressaa.dev@gmail.com>
2019-12-03 09:53:33 -0500

    Replacing font and not repeating background

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style/Header.css";
 const Header = () => (
   <div className="Header">
-    <h1>Star Wars Characters</h1>
+    <h1>STAR WARS CHARACTERS</h1>
   </div>
 );
 
